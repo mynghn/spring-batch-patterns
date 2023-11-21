a0699993aa2d6a1ea4b80ab8f5d23cc5bf5fbc6e
Myounghoon Lee <l.mynghn@gmail.com>
2023-11-21 18:28:24 +0900

    :tada: Scaffold project w/ Spring Initializr

---
+++ src/main/java/mynghn/springbatchpatterns/SpringBatchPatternsApplication.java
@@ -0,0 +1,13 @@
+package mynghn.springbatchpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchPatternsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mynghn/springbatchpatterns/SpringBatchPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package mynghn.springbatchpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
